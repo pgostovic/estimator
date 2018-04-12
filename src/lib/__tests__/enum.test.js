2407157ce1a06fd20d3c3b1d96d7ffe430ccf92e
Patrick Gostovic <pg@phranq.com>
2018-04-12 15:11:01 -0400

    Tests for enum.js

---
+++ src/lib/__tests__/enum.test.js
@@ -0,0 +1,30 @@
+import { makeEnum } from '../enum';
+
+test('array enum', () => {
+  const en = makeEnum('ONE', 'TWO', 'THREE');
+  expect(en.ONE).toBe('ONE');
+  expect(en.TWO).toBe('TWO');
+  expect(en.THREE).toBe('THREE');
+});
+
+test('object enum', () => {
+  const en = makeEnum({
+    ONE: 'UNO',
+    TWO: 'DOS',
+    THREE: 'TRES',
+  });
+  expect(en.ONE).toBe('UNO');
+  expect(en.TWO).toBe('DOS');
+  expect(en.THREE).toBe('TRES');
+});
+
+test('changing enum throws', () => {
+  const en = makeEnum('ONE', 'TWO', 'THREE');
+  expect(() => {
+    en.ONE = 'SEVEN';
+  }).toThrow();
+
+  expect(() => {
+    en.FOUR = 'FOUR';
+  }).toThrow();
+});
