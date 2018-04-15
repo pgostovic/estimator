e2474fa0e9829fb657920d65435e5fcd0a21176a
Patrick Gostovic <pg@phranq.com>
2018-04-15 08:47:04 -0400

    Added some comments.

--- src/lib/redux-util/makeReducer.js
+++ src/lib/redux-util/makeReducer.js
@@ -1,3 +1,8 @@
+/**
+ * Simple utility that lets you write your reducer as an object with action
+ * type keys and reducer function values instead of the more traditional
+ * switch/case way.
+ */
 
 export default (reducerObj, defaultState) =>
   (state = defaultState, action) =>
--- src/lib/styled.js
+++ src/lib/styled.js
@@ -1,3 +1,19 @@
+/**
+ * This is a wrapper around Styled Components that prefixes the generated
+ * selectors with the configured app root selector. This is needed to accomodate
+ * the fairly specific requirement of embedding in a 3rd party website. In order
+ * to quarantine this app's style, a scoped "css reset" is done from the
+ * configured app root as follows:
+ *
+ *    #${__APP__.root} {
+ *      ...reset css code...
+ *    }
+ * Without the applying this prefixed wrapper, the specificity of the styled
+ * components css would be such that the reset css took precendence. This
+ * wrapper allows styles to be overridden without required the !important
+ * directive.
+ */
+
 import styled, { injectGlobal as injectGlobalOrig } from 'styled-components';
 
 const wrap = args => {
