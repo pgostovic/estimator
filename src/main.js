f3f237b606c13a490c7d370a41d2f1bfbbab1a50
Patrick Gostovic <pg@phranq.com>
2018-04-18 08:51:50 -0400

    Added some polyfills for IE9 support

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
+// Polyfills that give us IE9 and up
+import 'core-js/es6/map';
+import 'core-js/es6/set';
+import 'raf/polyfill';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
