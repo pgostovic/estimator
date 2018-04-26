8b25b8701eb3fe46c78a2831467c45ff449c7c4b
Patrick Gostovic <pg@phranq.com>
2018-04-26 10:06:41 -0400

    Tests for phone formatting

--- src/app/ui/common/__tests__/input.test.js
+++ src/app/ui/common/__tests__/input.test.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import Adapter from 'enzyme-adapter-react-16';
+import { mount, configure } from 'enzyme';
 import snap from '../../../../lib/test-utils/snap';
 import Input from '../input';
+
+configure({ adapter: new Adapter() });
 
 snap('snapshot text default', (
   <Input type="text" />
@@ -9,3 +13,29 @@ snap('snapshot text default', (
 snap('snapshot text with suffix', (
   <Input type="text" suffix="Watts" />
 ));
+
+test('type=tel formatting (all numbers)', () => {
+  const onChange = jest.fn();
+  const wrapper = mount((
+    <Input type="tel" onChange={onChange} />
+  ));
+  wrapper.find('input').simulate('change', { target: { value: '4168885555' } });
+  expect(onChange).toBeCalledWith(expect.objectContaining({
+    target: {
+      value: '416-888-5555',
+    },
+  }));
+});
+
+test('type=tel formatting (numbers and non-numbers)', () => {
+  const onChange = jest.fn();
+  const wrapper = mount((
+    <Input type="tel" onChange={onChange} />
+  ));
+  wrapper.find('input').simulate('change', { target: { value: 'abc1234' } });
+  expect(onChange).toBeCalledWith(expect.objectContaining({
+    target: {
+      value: '123-4',
+    },
+  }));
+});
