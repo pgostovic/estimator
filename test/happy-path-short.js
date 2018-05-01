a078a0c1eb8a1f8e48130f57160e429894e71865
Patrick Gostovic <pg@phranq.com>
2018-05-01 11:21:46 -0400

    Added some "happy path" ui automation.

--- test/happy-path-short.js
+++ test/happy-path-short.js
@@ -1,6 +1,8 @@
 import { Selector } from 'testcafe';
 
-fixture`Getting Started`
+/* global fixture */
+
+fixture`Happy Path - short`
   .page`http://localhost:8080/`;
 
 const yearSelect = Selector('select[name=year]');
@@ -13,13 +15,9 @@ test('My Test', async t => {
   await t.click('#go');
 
   await t.click(yearSelect).click(yearSelect.find('option').nth(1));
-
   await t.click(makeSelect).click(makeSelect.find('option').nth(1));
-
   await t.click(modelSelect).click(modelSelect.find('option').nth(1));
-
   await t.click(trimSelect).click(trimSelect.find('option').nth(1));
-
   await t.typeText(mileageInput, '100000');
 
   await t.click('.calculateButton');
